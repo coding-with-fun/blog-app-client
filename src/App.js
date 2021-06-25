9e3071cb1f48945f601583c1189d15cf97a153e4
Harsh Patel <harsh.coderc@gmail.com>
2021-06-25 21:48:06 +0530

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
     return (
         <div>
-            <h1>Hello World!</h1>
+            <Navbar />
+
+            <div className="screens__container">
+                <HomeScreen />
+            </div>
         </div>
     );
 };
---
+++ src/components/Navbar.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">
+                    Blog.It
+                </a>
+
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div
+                    className="collapse navbar-collapse justify-content-end"
+                    id="navbarNav"
+                >
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a
+                                className="nav-link active"
+                                aria-current="page"
+                                href="/"
+                            >
+                                Home
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">
+                                Create New Post
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">
+                                About
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">
+                                Sign Out
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
