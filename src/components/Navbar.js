827f2b70bab7021d1361aeee4f48d53f93ea4cdf
Harsh Patel <harsh.coderc@gmail.com>
2021-06-26 13:56:05 +0530

    Added Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,104 +1,105 @@
-import React from "react";
-import { Link, useHistory, useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink, useHistory } from "react-router-dom";
 
 const Navbar = () => {
-    const location = useLocation();
     const history = useHistory();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const handleToggleMenu = () => setIsMenuOpen(!isMenuOpen);
 
     const handleSignOut = () => {
         history.push("/");
-        handleNavbarCollapse();
-    };
-
-    const handleNavbarCollapse = () => {
-        document.querySelector("#navbarNav").classList.remove("show");
+        handleToggleMenu();
     };
 
     return (
-        <nav className="navbar fixed-top navbar-expand-lg user-select-none navbar-light bg-light">
-            <div className="container-fluid">
-                <Link
-                    className="navbar-brand"
-                    onClick={handleNavbarCollapse}
-                    to="/"
-                >
+        <nav className="navbar">
+            <div className="nav-container">
+                <NavLink exact to="/" className="nav-logo">
                     Blog.It
-                </Link>
+                </NavLink>
 
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNav"
-                    aria-controls="navbarNav"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+                <ul className={`nav-menu ${isMenuOpen && "active"}`}>
+                    <li className="nav-item">
+                        <NavLink
+                            exact
+                            to="/"
+                            activeClassName="active"
+                            className="nav-link"
+                            onClick={handleToggleMenu}
+                        >
+                            Home
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink
+                            exact
+                            to="/create"
+                            activeClassName="active"
+                            className="nav-link"
+                            onClick={handleToggleMenu}
+                        >
+                            Create
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink
+                            exact
+                            to="/about"
+                            activeClassName="active"
+                            className="nav-link"
+                            onClick={handleToggleMenu}
+                        >
+                            About
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink
+                            exact
+                            to="/profile"
+                            activeClassName="active"
+                            className="nav-link"
+                            onClick={handleToggleMenu}
+                        >
+                            Profile
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <div className="nav-link" onClick={handleSignOut}>
+                            Sign Out
+                        </div>
+                    </li>
+                    <li className="nav-item"></li>
+                </ul>
 
-                <div
-                    className="collapse navbar-collapse justify-content-end"
-                    id="navbarNav"
-                >
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <Link
-                                className={`nav-link ${
-                                    location.pathname === "/" && "active"
-                                }`}
-                                aria-current={
-                                    location.pathname === "/" ? "page" : "false"
-                                }
-                                onClick={handleNavbarCollapse}
-                                to="/"
-                            >
-                                Home
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link
-                                className={`nav-link ${
-                                    location.pathname === "/create" && "active"
-                                }`}
-                                aria-current={
-                                    location.pathname === "/create"
-                                        ? "page"
-                                        : "false"
-                                }
-                                onClick={handleNavbarCollapse}
-                                to="/create"
-                            >
-                                Create New Post
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link
-                                className={`nav-link ${
-                                    location.pathname === "/about" && "active"
-                                }`}
-                                aria-current={
-                                    location.pathname === "/about"
-                                        ? "page"
-                                        : "false"
-                                }
-                                onClick={handleNavbarCollapse}
-                                to="/about"
-                            >
-                                About
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <span
-                                className="nav-link"
-                                onClick={handleSignOut}
-                                role="button"
-                                tabIndex="0"
-                            >
-                                Sign Out
-                            </span>
-                        </li>
-                    </ul>
+                <div className="nav-icon" onClick={handleToggleMenu}>
+                    {isMenuOpen ? (
+                        <svg
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41L17.59 5Z"
+                                fill="#000"
+                            ></path>
+                        </svg>
+                    ) : (
+                        <svg
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M21 18H3V16H21V18ZM21 13H3V11H21V13ZM21 8H3V6H21V8Z"
+                                fill="#000"
+                            ></path>
+                        </svg>
+                    )}
                 </div>
             </div>
         </nav>
--- src/utils/WrappedRouter.js
+++ src/utils/WrappedRouter.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import AboutScreen from "../screens/AboutScreen";
 import HomeScreen from "../screens/HomeScreen";
 import NewPostScreen from "../screens/NewPostScreen";
+import ProfileScreen from "../screens/ProfileScreen";
 
 const WrappedRouter = () => {
     return (
@@ -10,6 +11,7 @@ const WrappedRouter = () => {
             <Route exact path="/" component={HomeScreen} />
             <Route exact path="/create" component={NewPostScreen} />
             <Route exact path="/about" component={AboutScreen} />
+            <Route exact path="/profile" component={ProfileScreen} />
         </Switch>
     );
 };
