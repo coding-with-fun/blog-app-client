c09c79454542d2adb8bde6e0c899325eff1c5ff2
Harsh Patel <harsh.coderc@gmail.com>
2021-06-26 23:20:41 +0530

    Initiated post lists on Home Page

--- src/contexts/PostDataContext.js
+++ src/contexts/PostDataContext.js
@@ -1,13 +1,15 @@
 import React, { createContext, useState } from "react";
+import SampleData from "../sampleData.json";
 
 export const PostDataContext = createContext();
 
 export const PostDataProvider = ({ children }) => {
-    const [postsList, setPostsList] = useState([]);
+    const [postsList, setPostsList] = useState(SampleData);
 
     const addPost = (data) => {
         const postsListCopy = [...postsList];
         const postData = {
+            key: postsListCopy.length,
             title: data.title,
             content: data.content,
         };
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { PostDataContext } from "../contexts/PostDataContext";
 
 const HomeScreen = () => {
+    const { postsList } = useContext(PostDataContext);
+    console.log(postsList);
+
     return (
-        <div>
-            <h1>Home Screen</h1>
+        <div className="container posts_list__container">
+            {postsList.map((post) => {
+                return (
+                    <div className="individual_post" key={post.key}>
+                        <div>{post.title}</div>
+
+                        <div>{post.content}</div>
+                    </div>
+                );
+            })}
         </div>
     );
 };
